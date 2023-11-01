06544b623123eeb0c71b020ddc2a10e57c450b2a
unknown <rahulsharma546558@gmail.com>
2023-11-01 04:40:22 -0700

    first commit

---
+++ css/dpconnection.php/dpconnection.php
@@ -0,0 +1,16 @@
+<?php
+   $db=mysqli_connect("localhost","root","","debra1122","3306");
+
+   if(!$db)
+   {
+   ?>
+           <script language="javascript">
+          alert("Error: Unable to connect Database");
+    <script>
+    <?php
+     }
+     ?>
+
+
+
+
